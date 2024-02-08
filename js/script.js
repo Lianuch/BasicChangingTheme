7958333ff7361afb7caea8814b91f39ebc7caa25
Lianuch <a.nyzhnyk@nltu.lviv.ua>
2024-02-08 20:27:48 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let toggleTheme = document.getElementById('toggleTheme')
+
+toggleTheme.onclick = () => {
+    if(document.documentElement.hasAttribute('theme')){
+        document.documentElement.removeAttribute('theme');
+        toggleTheme.value = "Dark"
+    }
+    else{
+        document.documentElement.setAttribute('theme','dark');
+        toggleTheme.value = "Bright"
+
+    }
+}
+
